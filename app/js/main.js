d91d11f940841bd2765bdf2bdbc957bf3595a906
Cori Fowler <fowler.cori@gmail.com>
2015-12-28 22:03:05 -0500

    basic functionality of adding/deleting/updating tasks

--- app/js/main.js
+++ app/js/main.js
@@ -4,14 +4,65 @@
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
-var TaskController = function TaskController() {
+var TaskController = function TaskController($scope) {
 
   var vm = this;
 
-  console.log('hello, world');
+  vm.editing = false;
+
+  // empty task list
+
+  vm.taskList = [];
+
+  // declare functions for adding, updating, deleting tasks
+
+  vm.addTask = addTask;
+  vm.editTask = editTask;
+  vm.updateTask = updateTask;
+  vm.deleteTask = deleteTask;
+
+  // functions
+
+  function addTask(task) {
+    task.done = false;
+    vm.taskList.push(task);
+
+    // clear form
+
+    vm.task = '';
+  }
+
+  function editTask(t) {
+    vm.editing = true;
+
+    // prefill form
+    vm.task = t;
+  }
+
+  function updateTask() {
+    vm.editing = false;
+    vm.task = '';
+  }
+
+  function deleteTask(task) {
+    task.done = true;
+    var allTasks = vm.taskList;
+    vm.taskList = [];
+
+    // only add unfinished tasks to list, remove finished
+    angular.forEach(allTasks, function (task) {
+      if (task.done === false) {
+        vm.taskList.push(task);
+      }
+
+      // reset form
+      vm.editing = false;
+      vm.task = '';
+    });
+  }
 };
 
-TaskController.$inject = [];
+TaskController.$inject = ['$scope'];
 
 exports['default'] = TaskController;
 module.exports = exports['default'];
--- js/controllers/taskcontroller.js
+++ js/controllers/taskcontroller.js
@@ -1,11 +1,63 @@
-let TaskController = function() {
+let TaskController = function($scope) {
   
   let vm = this;
 
-  console.log('hello, world');
+  vm.editing = false;
+
+  // empty task list
+
+  vm.taskList = [];
+
+  // declare functions for adding, updating, deleting tasks
+
+  vm.addTask = addTask;
+  vm.editTask = editTask;
+  vm.updateTask = updateTask;
+  vm.deleteTask = deleteTask;
+
+  // functions
+
+  function addTask (task) {
+    task.done = false;
+    vm.taskList.push(task);
+
+    // clear form
+
+    vm.task = '';
+  }
+
+  function editTask (t) {
+    vm.editing = true;
+
+    // prefill form
+    vm.task = t;
+  }
+
+  function updateTask () {
+    vm.editing = false;
+    vm.task = '';
+  }
+
+  function deleteTask (task) {
+    task.done = true;
+    let allTasks = vm.taskList;
+    vm.taskList = [];
+
+    // only add unfinished tasks to list, remove finished
+    angular.forEach(allTasks, function(task) {
+      if (task.done === false) {
+        vm.taskList.push(task);
+      }
+
+      // reset form
+      vm.editing = false;
+      vm.task = '';
+    });
+  }
+
 
 };
 
-TaskController.$inject = [];
+TaskController.$inject = ['$scope'];
 
 export default TaskController;
